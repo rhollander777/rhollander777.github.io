a65a08d6e0f56b1497a2c8cb459b3a7934614200
rhollander777 <32153627+rhollander777@users.noreply.github.com>
2017-11-11 15:28:27 -0700

    Add files via upload

--- js/flowerShop.js
+++ js/flowerShop.js
@@ -52,9 +52,9 @@
 			'<td id="code' + rCnt + '">' + arr.flower[i].Code + '</td>' +
 			'<td id="price' + rCnt + '">$' + arr.flower[i].Price +  '</td>' +
 			'<td>' +
-		  	'<input type="button" id="addBtn' + rCnt +  '" name="addBtn' + rCnt + '" class="btn" value="Add" onclick="addToCart(' + arr.flower[i].id + ', "' + arr.flower[i].Name + '", "' + arr.flower[i].Code + '", 1, ' + arr.flower[i].Price + ')" /> </td>' +
+		  	'<input type="button" id="addBtn' + rCnt +  '" name="addBtn' + rCnt + '" class="btn" value="Add" onclick="addToCart(' + arr.flower[i].id + ', ' + "'" + arr.flower[i].Name + "'" + ', ' + "'" + arr.flower[i].Code + "'" + ', 1, ' + arr.flower[i].Price + ')" /> </td>' +
 			'<td>' +
-			'<input type="button" id="delBtn' + rCnt + '" class="delBtn' + rCnt + '" class="btn" value="Remove" onclick="removeFromCart(' + arr.flower[i].id + ', "' + arr.flower[i].Name + '", "' + arr.flower[i].Code + '", 1, ' + arr.flower[i].Price + ')" />' + 
+			'<input type="button" id="delBtn' + rCnt + '" class="delBtn' + rCnt + '" class="btn" value="Remove" onclick="removeFromCart(' + arr.flower[i].id + ', ' + "'" + arr.flower[i].Name + "'" + ', ' + "'" + arr.flower[i].Code + "'" + ', 1, ' + arr.flower[i].Price + ')" />' + 
 			'</td></tr>'; rCnt++;
 	 }
 		console.log(vOut);
