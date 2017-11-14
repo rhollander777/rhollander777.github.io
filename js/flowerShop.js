dca34de79a06fb7fd7bec0006fcd341470609b83
rhollander777 <32153627+rhollander777@users.noreply.github.com>
2017-11-14 05:38:46 -0700

    Add files via upload

--- js/flowerShop.js
+++ js/flowerShop.js
@@ -165,15 +165,22 @@
 		var vCart = JSON.parse(localStorage.getItem('sCart'));
 		console.log("sCart returned = " + vCart);
 		var vItemCnt = 0;
-		var vOut = '<p style="font-size:18px;"><b>Shopping Cart</b></p><table><th>Item</th><th>Product Description</th><th>Code</th><th>Qty</th><th>Price</th><th>Cost</th>';
-		for (i = 0; i < vCart.length; i++) {
-			vOut += '<tr><td>' + vCart.sItem[i][i] + '</td>';
-			for (vItemCnt = 0; vCart.sItem.length; vItemCnt++) {
-				vOut += '<td>' + vCart.sItem[i][vItemCnt] + '</td>';				
+		var vOut = "";
+		if (vCart.length = 0) {
+			vOut = "Empty";
+		} else {
+			vOut = '<p style="font-size:18px;"><b>Shopping Cart</b></p><table><th>Item</th><th>Product Description</th><th>Code</th><th>Qty</th><th>Price</th><th>Cost</th>';
+			for (i = 0; i < vCart.length; i++) {
+				vOut += '<tr>';
+				for (vItemCnt = 0; vCart.sItem.length; vItemCnt++) {
+					vOut += '<td>' + vCart[0].sItem[i][vItemCnt] + '</td>';				
+				}
+				vOut += '</td>';
 			}
-		}
-		vOut += '</tr></table>';
-		document.getElementById("displayCart").innerHTML = vOut;
+			vOut += '</tr></table>';
+		}	
+		console.log("getCart vOut = " + vOut);
+//		document.getElementById("displayCart").innerHTML = vOut;
 		return false;
 	}
 	
