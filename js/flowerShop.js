bead0018babeb86ca220aa77bd9f3cfe65b797b5
rhollander777 <32153627+rhollander777@users.noreply.github.com>
2017-11-20 22:16:52 -0700

    Add files via upload

--- js/flowerShop.js
+++ js/flowerShop.js
@@ -166,9 +166,9 @@
 			console.log("sItemCnt= " + sItemCnt);
 */
 			sCart.sItem.push(newItem);
-			localStorage.setItem('sCart', JSON.stringify(sCart));
-			console.log("newly stored sCart = " + JSON.parse(localStorage.getItem('sCart')));
-			vCart = JSON.parse(localStorage.getItem('sCart'));
+			localStorage.setItem('sItem', JSON.stringify(sCart));
+			console.log("newly stored sCart = " + JSON.parse(localStorage.getItem('sItem')));
+			vCart = JSON.parse(localStorage.getItem('sItem'));
 			console.log("id = " + vCart.sItem[0].id);
 			sItemCnt++;
 			console.log("sItemCnt = " + sItemCnt);
